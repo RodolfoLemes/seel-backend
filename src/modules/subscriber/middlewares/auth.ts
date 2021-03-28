7eeeec925d78112b40023ee794d779d0454927c0
Rodolfo Lemes <rodolfo_fero@hotmail.com>
2021-03-28 20:01:33 -0300

    feat, refactor: add confirm payment service, add auth middleware

---
+++ src/modules/subscriber/middlewares/auth.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+import authConfig from '@config/auth';
+import AppError from '@shared/errors/AppError';
+
+export default function auth(
+  req: Request,
+  _res: Response,
+  next: NextFunction,
+): void {
+  const { secret } = req.body;
+
+  if (secret !== authConfig.secret) throw new AppError('Forbidden', 403);
+
+  next();
+}
